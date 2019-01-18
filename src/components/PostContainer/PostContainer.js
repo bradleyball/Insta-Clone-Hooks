3103f768d7ac21bf833f28e605450b559d4aaf61
cacheUp <bradb26@gmail.com>
2019-01-18 08:21:16 +0300

    Refactored components inside of PostContainer

---
+++ src/components/PostContainer/PostContainer.js
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-unused-vars */
+/**@jsx jsx */
+
+import React from "react";
+import PostHeader from "./PostHeader";
+import PostPicture from "./PostPicture";
+import PostIcons from "./PostIcons";
+import CommentContainer from "../CommentSection/CommentContainer";
+import { css, jsx } from "@emotion/core";
+
+const styledPostContainer = css`
+  display: grid;
+  width: 550px;
+  border: 1px solid black;
+`;
+
+const styledCommentContainer = css`
+  display: grid;
+  justify-content: flex-start;
+`;
+
+function PostContainer(props) {
+  return (
+    <div css={styledPostContainer}>
+      <PostHeader data={props.data} />
+      <PostPicture data={props.data} />
+      <PostIcons data={props.data} addLike={props.addLike} />
+      <div css={styledCommentContainer}>
+        <CommentContainer data={props.data} />
+      </div>
+    </div>
+  );
+}
+
+export default PostContainer;
